4d90e8a5b0e7ce86ac93f417b2ad5f0fd3eb5f89
EC2 Default User <testtest@gmail.com>
2018-07-31 12:48:21 +0000

    Procfile

--- resources/views/messages/create.blade.php
+++ resources/views/messages/create.blade.php
@@ -3,24 +3,23 @@
 @section('content')
 
  <h1>メッセージ新規作成ページ</h1>
-
-    @if (count($errors) > 0)
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    @endif
-
-
-    {!! Form::model($message, ['route' => 'messages.store']) !!}
-
-        {!! Form::label('content', 'メッセージ:') !!}
-        {!! Form::text('content') !!}
-
-        {!! Form::submit('投稿') !!}
-
-    {!! Form::close() !!}
-
-
+    <div class="row">
+        <div class="col-xs-6"> 
+            {!! Form::model($message, ['route' => 'messages.store']) !!}
+                
+            <div class="form-group">
+                {!! Form::label('title', 'タイトル:') !!}
+                {!! Form::text('title', null, ['class' => 'form-control']) !!}
+            </div>
+            
+            <div class="form-group">
+                {!! Form::label('content', 'メッセージ:') !!}
+                {!! Form::text('content', null, ['class' => 'form-control']) !!}
+            </div>
+        
+            {!! Form::submit('投稿') !!}
+    
+        {!! Form::close() !!}
+        </div>
+    </div>
 @endsection
